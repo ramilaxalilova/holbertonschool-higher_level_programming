df0837f48be2ad3d8874f5a8701ddcbf03c8c880
ramilaxalilova <xalilovaramila@yahoo.com>
2024-01-25 08:15:08 -0800

    9-error

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    ld = abs(number % 10)
+    if number < 0:
+        ld = -number % 10
+    else:
+        ld = number % 10
 
     print(ld, end="")
     return ld
