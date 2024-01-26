4a2822e1c8d53d9dc4843a20d372f10d242af3d6
ramilaxalilova <xalilovaramila@yahoo.com>
2024-01-26 03:24:51 -0800

    argv

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+    count = len(argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, argv[i + 1]))
