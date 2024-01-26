b5f277b7531e76431e6c225a48c36c0924de47bc
ramilaxalilova <xalilovaramila@yahoo.com>
2024-01-26 03:53:15 -0800

    infinite

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    count = len(argv) - 1
+    result= 0
+    for i in range(count):
+        result += int(argv[i + 1])
+    print("{}".format(result))
