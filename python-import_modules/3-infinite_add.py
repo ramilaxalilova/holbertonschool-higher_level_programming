d4c6237de0d71479d9bcb8f5b2eb1ec7e1732849
ramilaxalilova <xalilovaramila@yahoo.com>
2024-01-26 06:42:51 -0800

    pycde

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ from sys import argv
 
 if __name__ == "__main__":
     count = len(argv) - 1
-    result= 0
+    result = 0
     for i in range(count):
         result += int(argv[i + 1])
     print("{}".format(result))
