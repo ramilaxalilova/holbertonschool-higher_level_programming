c40383fe48ccdedf2f3b1d920dc21c265aa79099
ramilaxalilova <xalilovaramila@yahoo.com>
2024-01-27 02:15:33 -0800

    matric

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
-        n = 0;
+        n = 0
         for j in matrix[i]:
-            print("{:d}".format(j),end="")
+            print("{:d}".format(j), end="")
             if n < len(matrix[i]) - 1:
                 print(end=" ")
             n += 1
