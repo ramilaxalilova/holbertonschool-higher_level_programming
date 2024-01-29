5c92583ee5587de0869c2aa575ff8fa191a77b78
ramilaxalilova <xalilovaramila@yahoo.com>
2024-01-28 22:55:49 -0800

    error

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    if matrix is None:
+        matrix = []
+
     sqrdm = list(map(lambda i: list(map(lambda j: (j**2), i)), matrix))
     return(sqrdm)
