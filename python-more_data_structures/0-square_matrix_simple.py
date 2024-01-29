f87e30329dc47ab1e108b11e37ca142a6e042557
ramilaxalilova <xalilovaramila@yahoo.com>
2024-01-28 23:09:05 -0800

    saf

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,5 @@
 def square_matrix_simple(matrix=[]):
     if matrix is None:
         matrix = []
-
-    sqrdm = list(map(lambda i: list(map(lambda j: (j**2), i)), matrix))
-    return(sqrdm)
+    sqrdm = list(map(lambda row: list(map(lambda j: (j**2), row)), matrix))
+    return sqrdm
