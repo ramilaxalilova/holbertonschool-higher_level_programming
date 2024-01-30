9307357b572641efac280ceeba7d17a7224b924c
ramilaxalilova <xalilovaramila@yahoo.com>
2024-01-30 00:34:53 -0800

    error

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except (ValueError, TypeError, IndexError):
-            break
+        except (ValueError, TypeError):
+            continue
         else:
             count += 1
     print()
