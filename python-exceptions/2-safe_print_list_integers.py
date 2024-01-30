56a5c01e8ff330d0ea563ecfc6fcca185d2f9603
ramilaxalilova <xalilovaramila@yahoo.com>
2024-01-30 00:32:43 -0800

    aer

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except (ValueError, TypeError, IndexError):
-            continue
+        except:
+            break
         else:
             count += 1
     print()
