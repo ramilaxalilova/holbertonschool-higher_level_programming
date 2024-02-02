e5914c0ec8ce336877d813352c234ab36f14ae89
ramilaxalilova <xalilovaramila@yahoo.com>
2024-02-02 03:59:06 -0800

    khsbef

---
+++ python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""next step in square class"""
+
+
+class Square:
+    """Square class with private attribute size"""
+    def __init__(self, size=0):
+        """Initialize Square with size attribute"""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Public instance method that returns the current square area."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
