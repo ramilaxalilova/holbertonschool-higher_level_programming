b3d1db9652b6e0eb3b89439e1be9db4a675adf36
ramilaxalilova <xalilovaramila@yahoo.com>
2024-02-01 23:15:03 -0800

    imort size

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square. """
+    
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
