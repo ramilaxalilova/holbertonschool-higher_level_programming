0a4862d3884adc543e69f168de220daf07abc142
ramilaxalilova <xalilovaramila@yahoo.com>
2024-02-05 03:42:30 -0800

    zefsg

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """rectangle"""
     def __init__(self, width=0, height=0):
+        """initialize width and height"""
         self.width = width
         self.height = height
 
