c531ef0d30902a14585dd989437d97f2d633150b
ramilaxalilova <xalilovaramila@yahoo.com>
2024-02-06 00:08:29 -0800

    df

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,12 +11,10 @@ class Rectangle:
 
     @property
     def width(self):
-        """get width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """sets width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -25,14 +23,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """defines height as private"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """set height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be an integer")
+            raise ValueError("height must be >= integer")
         self.__height = value
