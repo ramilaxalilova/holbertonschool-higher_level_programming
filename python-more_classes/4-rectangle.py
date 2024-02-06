5c47b43051533d26d617904906ca2a74190c748f
ramilaxalilova <xalilovaramila@yahoo.com>
2024-02-06 01:25:43 -0800

    eval

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""adding rectangle area and parameter to class as string"""
+
+
+class Rectangle:
+    """adding rectangle area and parameter to class as string"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        sharpstr = ""
+        for _ in range(self.__height):
+            row = "#" * self.__width
+            sharpstr += row + "\n"
+        return sharpstr.rstrip()
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
