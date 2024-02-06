4a1ab68af7a2aa7d5688253a43b77dd136db91a1
ramilaxalilova <xalilovaramila@yahoo.com>
2024-02-06 02:55:09 -0800

    width

--- python-more_classes/9-main.py
+++ python-more_classes/9-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
 
-my_square = Rectangle.square("12")
-print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
+my_square = Rectangle.square("2")
+print("{} / {}".format(my_square.width, my_square.height))
 print(my_square)
