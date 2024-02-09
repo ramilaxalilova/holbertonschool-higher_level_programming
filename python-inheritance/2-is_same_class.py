143377bbb1f64705bb2ba57c6207268144dd6339
unknown <xalilovaramila@yahooo.com>
2024-02-09 11:54:57 +0400

    checktype

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -3,8 +3,7 @@
 
 
 def is_same_class(obj, a_class):
-    """something"""
-    if isinstance(obj, a_class):
+    """check type"""
+    if type(obj) == a_class:
         return True
-    else:
-        return False
+    return False
