8e73c913cd65b92b5b7972859f41635f6fd6127e
unknown <xalilovaramila@yahooo.com>
2024-02-09 11:22:19 +0400

    doc

--- python-inheritance/3-main.py
+++ python-inheritance/3-main.py
@@ -2,6 +2,7 @@
 is_kind_of_class = __import__('3-is_kind_of_class').is_kind_of_class
 
 a = 1
+
 if is_kind_of_class(a, int):
     print("{} comes from {}".format(a, int.__name__))
 if is_kind_of_class(a, float):
