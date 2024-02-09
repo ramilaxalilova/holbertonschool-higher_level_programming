d125606ffc7bd3dea59b419f662ea18ad5a209f3
unknown <xalilovaramila@yahooo.com>
2024-02-09 14:14:54 +0400

    9

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""create empty class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """create square based on rectangle"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
