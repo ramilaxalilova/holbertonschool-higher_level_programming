c8df929a19982701f8d9ffbd777200ebea777928
unknown <xalilovaramila@yahooo.com>
2024-02-11 00:26:20 +0400

    write1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,9 +2,8 @@
 """return len"""
 
 
-
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of characters written."""
+    """Writes a string to a text file (UTF8) and returns len"""
     with open(filename, 'w', encoding='utf-8') as file:
         file.write(text)
         return len(text)
