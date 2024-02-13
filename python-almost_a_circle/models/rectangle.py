cff5b25ac59479d5efc8908feacd7523f459fc1f
unknown <xalilovaramila@yahooo.com>
2024-02-13 12:31:34 +0400

    smthng

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -73,7 +73,7 @@ class Rectangle(Base):
             print('#' * self.width)
 
     def __str__(self):
-        """Return a string representationf rectangle."""
-
-        return ("[Rectangle] ({}) {}/{} - {}/{}"
-            .format(self.id, self.x, self.y, self.width, self.height))
+        """Return the print() and str() representation of the Rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
