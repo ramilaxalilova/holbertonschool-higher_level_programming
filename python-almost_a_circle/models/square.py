d451128f304631497829f7053548c3ec492c67f6
unknown <xalilovaramila@yahooo.com>
2024-02-13 13:11:46 +0400

    sqsqs

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,3 +11,14 @@ class Square(Rectangle):
     def __str__(self):
         return ("[Square] ({}) {}/{} - {}"
                 .format(self.id, self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        """Get the size of the Square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the Square."""
+        self.width = value
+        self.height = value
