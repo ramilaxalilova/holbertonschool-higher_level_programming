84ed883e24c8054d1c824099eeaacdb3c26029d0
unknown <xalilovaramila@yahooo.com>
2024-02-13 11:27:01 +0400

    display

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""rectangleeeeeeee"""
-from base import Base
+"""ractangle from base"""
+from models.base import Base
 
 
 class Rectangle(Base):
-    """RECTANGLE CLASSkmdkem"""
+    """RECTANGLE CLASS"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """..."""
+
         super().__init__(id)
         self.width = width
         self.height = height
@@ -14,10 +17,14 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """..."""
+
         return self.__width
 
     @width.setter
     def width(self, value):
+        """..."""
+
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value <= 0:
@@ -26,10 +33,14 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """..."""
+
         return self.__height
 
     @height.setter
     def height(self, value):
+        """..."""
+
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value <= 0:
@@ -38,10 +49,14 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """..."""
+
         return self.__x
 
     @x.setter
     def x(self, value):
+        """..."""
+
         if type(value) is not int:
             raise TypeError("x must be an integer")
         elif value < 0:
@@ -50,10 +65,14 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """..."""
+
         return self.__y
 
     @y.setter
     def y(self, value):
+        """..."""
+
         if type(value) is not int:
             raise TypeError("y must be an integer")
         elif value < 0:
@@ -66,7 +85,7 @@ class Rectangle(Base):
         return self.width * self.height
 
     def display(self):
-        """display with #"""
+        """"display with #"""
 
         for _ in range(self.height):
             print('#' * self.width)
