36311fe768390124d08bb4e7a81e7eec420c7717
unknown <xalilovaramila@yahooo.com>
2024-02-15 21:18:15 +0400

    test base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -13,6 +13,7 @@ class TestBase(unittest.TestCase):
 
     def test_id_as_positive(self):
         """test for pos id"""
+
         base_instance = Base(115)
         self.assertEqual(base_instance.id, 115)
         base_instance = Base(67)
@@ -47,24 +48,9 @@ class TestBase(unittest.TestCase):
         json_data = Base.to_json_string([rect_data])
         self.assertEqual(type(json_data), str)
 
-    def test_empty_to_json_string(self):
-        """test for empty json"""
-        empty_data = []
-        json_data = Base.to_json_string(empty_data)
-        self.assertEqual(json_data, "[]")
-
-        empty_data = None
-        json_data = Base.to_json_string(empty_data)
-        self.assertEqual(json_data, "[]")
-
-    def test_instance(self):
-        """Test a Base Class instance"""
-        base_instance = Base()
-        self.assertEqual(type(base_instance), Base)
-        self.assertTrue(isinstance(base_instance, Base))
-
     def test_to_json_string(self):
         """tests fucntion functionality"""
+
         rect_data = {'id': 31, 'x': 14, 'y': 11, 'width': 3, 'height': 3}
         json_data = Base.to_json_string([rect_data])
 
@@ -73,48 +59,4 @@ class TestBase(unittest.TestCase):
         self.assertCountEqual(
             json_data,
             '{["id": 31, "x": 14, "y": 11, "width": 3, "height": 3]}'
-        )
-
-    def test_wrong_to_json_string(self):
-        """Test a wrong functionality """
-        json_data = Base.to_json_string(None)
-        self.assertEqual(json_data, "[]")
-
-        warn = ("to_json_string() missing 1 required positional argument: " +
-                "'list_dictionaries'")
-
-        with self.assertRaises(TypeError) as msg:
-            Base.to_json_string()
-
-        self.assertEqual(warn, str(msg.exception))
-
-        warn = "to_json_string() takes 1 positional argument but 2 were given"
-
-        with self.assertRaises(TypeError) as msg:
-            Base.to_json_string([{43, 87}], [{22, 17}])
-
-        self.assertEqual(warn, str(msg.exception))
-
-    def test_wrong_save_to_file(self):
-        """
-        Test the save_to_file method
-        """
-        with self.assertRaises(AttributeError) as msg:
-            Base.save_to_file([Base(1), Base(2)])
-
-        self.assertEqual(
-             "'Base' object has no attribute 'to_dictionary'",
-             str(msg.exception)
-        )
-
-    def test_create(self):
-        """
-        Test the create method
-        """
-        with self.assertRaises(TypeError) as msg:
-            warn = Rectangle.create('Monty Python')
-
-        self.assertEqual(
-            "create() takes 1 positional argument but 2 were given",
-            str(msg.exception)
         )
