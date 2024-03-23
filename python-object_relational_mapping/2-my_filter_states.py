13ecb4ad371a7f74ba3c67c8501af1d52d9b8f57
ramilaxalilova <xalilovaramila@yahoo.com>
2024-03-23 11:15:28 +0000

    second task

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,28 @@
-dg
+#!/usr/bin/python3
+"""
+takes in an argument
+displays all values in the states table
+where name matches the argument
+"""
+import sys
+import MySQLdb
+
+
+def main():
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+    a = sys.argv[4]
+
+    db = MySQLdb.connect(user=u, passwd=p, db=d, host='localhost', port=3306)
+
+    cur = db.cursor()
+    q = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id".format(a)
+    cur.execute(q)
+    [print(state) for state in cur.fetchall()]
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
