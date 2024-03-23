28bc2ba44960d1f44a4410111cf23e300b0c3cac
ramilaxalilova <xalilovaramila@yahoo.com>
2024-03-23 11:20:07 +0000

    setw

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,25 @@
--- Create states table in hbtn_0e_4_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_4_usa;
-USE hbtn_0e_4_usa;
-CREATE TABLE IF NOT EXISTS states ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+#!/usr/bin/python3
+""" lists all cities from city table """
+import MySQLdb
+import sys
 
-CREATE TABLE IF NOT EXISTS cities ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY(state_id) REFERENCES states(id)
-);
-INSERT INTO cities (state_id, name) VALUES (1, "San Francisco"), (1, "San Jose"), (1, "Los Angeles"), (1, "Fremont"), (1, "Livermore");
-INSERT INTO cities (state_id, name) VALUES (2, "Page"), (2, "Phoenix");
-INSERT INTO cities (state_id, name) VALUES (3, "Dallas"), (3, "Houston"), (3, "Austin");
-INSERT INTO cities (state_id, name) VALUES (4, "New York");
-INSERT INTO cities (state_id, name) VALUES (5, "Las Vegas"), (5, "Reno"), (5, "Henderson"), (5, "Carson City");
+
+def main():
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+    db = MySQLdb.connect(user=u, passwd=p, db=d, host='localhost', port=3306)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT c.id, c.name, s.name FROM cities c \
+                LEFT JOIN states s ON c.state_id = s.id \
+                ORDER BY c.id")
+
+    [print(state) for state in cur.fetchall()]
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
