7d707ba0a2dfdb683bec8ebd4581faccd198780e
ramilaxalilova <xalilovaramila@yahoo.com>
2024-03-23 11:03:36 +0000

    first task

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" lists all states from state table """
+import MySQLdb
+import sys
+
+
+def main():
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+    db = MySQLdb.connect(user=u, passwd=p, db=d, host='localhost', port=3306)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id")
+
+    [print(state) for state in cur.fetchall()]
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
